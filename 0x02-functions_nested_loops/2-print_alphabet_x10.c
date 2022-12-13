ae8532180c8baf403395e15665f179ed9c218b1e
GhassanTeto <ghassanteto19@gmail.com>
2022-12-13 09:07:23 -0800

    t2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,15 @@
 
 /**                                                                        
  *
- * print_alpabet_x10 - function to print abc 10 times                                   
+ * print_alpabet_x10 -> function to print abc 10 times                                   
  *                                                                   
  * Return: 0                                                           
  */                                                                        
 
 void print_alphabet(void)                                                  
 {                                                                        
-	        char c, i;
+	        int c, i;
+
 		for(i = 0; i <= 9; i++)
 		{
 			
