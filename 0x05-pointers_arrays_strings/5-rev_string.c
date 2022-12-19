8a7573a917614360ab030ed9745efb11e964c943
GhassanTeto <ghassanteto19@gmail.com>
2022-12-19 05:41:11 -0800

    67

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string 
+ * rev_string - reverses a string
  * @s: input
  * Return: string in reverse
  */
@@ -20,4 +20,4 @@ void rev_string(char *s)
 		s[i] = s[c];
 		s[c] = rv;
 	}
-}	
+}
