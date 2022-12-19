35fd2f3358da7b57e5a2c34735cf59c1e94b672c
GhassanTeto <ghassanteto19@gmail.com>
2022-12-19 04:22:25 -0800

    66

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <string.h>
+
 /**
  * puts2 -> puts2 function
  * @str: str parameter
  */
 void puts2(char *str)
 {
-	int len;
+	int len, i;
 
 	len = strlen(str);
 	for (i = 0; i < len; i += 2)
