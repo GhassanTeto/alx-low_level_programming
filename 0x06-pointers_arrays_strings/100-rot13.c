a3a385f6d9df1fc12f55277db28b6dab4eb6f2f0
GhassanTeto <ghassanteto19@gmail.com>
2022-12-22 02:35:28 -0800

    t10

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * *rot13 - function that string using rot13.
+ * *rot13 - function that string using rot13
  * @str: string
- * Return: encoded string.
+ *Return: encoded string
  */
 char *rot13(char *str)
 {
 	int x, y;
-
 	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdfghijklmnopqrstuvwxy";
 	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdfghijklm";
-	
+
 	for (x = 0; str[x] != '\0'; x++)
 	{
 		for (y = 0; input[y] != '\0'; y++)
