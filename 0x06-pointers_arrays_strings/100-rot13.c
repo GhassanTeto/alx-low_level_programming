3952b796fda2a98a4ecd15e4fc15f68b56ad1209
GhassanTeto <ghassanteto19@gmail.com>
2022-12-22 02:31:28 -0800

    10

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
- *
- * Return: A pointer to the encoded string.
+ * *rot13 - function that string using rot13.
+ * @str: string
+ * Return: encoded string.
  */
 char *rot13(char *str)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	int x, y;
 
-while (str[indx1])
-{
-	for (indx2 = 0; indx2 < 52; indx2++)
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdfghijklmnopqrstuvwxy";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdfghijklm";
+	
+	for (x = 0; str[x] != '\0'; x++)
 	{
-		if (str[indx1] == alphabet[indx2])
+		for (y = 0; input[y] != '\0'; y++)
 		{
-			str[indx1] = rot13key[indx2];
-			break;
+			if (str[x] == input[y])
+			{
+				str[x] = output[y];
+				break;
+			}
 		}
 	}
-	indx1++;
-}
-
-return (str);
+	return (str);
 }
