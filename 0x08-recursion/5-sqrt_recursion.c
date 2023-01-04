5fbff68c34d971095399891ff4215fa9ece2027e
GhassanTeto <ghassanteto19@gmail.com>
2023-01-04 14:15:16 -0800

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,19 @@
-/*
- * File: 5-sqrt_recursion.c
- * Auth: me
- */
-
 #include "main.h"
-
-int find_sqrt(int num, int root);
-int _sqrt_recursion(int n);
-
 /**
- * find_sqrt - Finds the natural square of an inputted number.
- * @num: The number to find the square root of.
- * @root: The root to be tested.
- *
- * Return: If the number has a natural square root - the square root.
- * 	   If the number does not have a natural square root - -1.
+ * _evaluate - evaluate function sqrt
+ * @i: interger
+ * @n: interger
+ * Return: evaluate sqrt
  */
-int find_sqrt(int num, int root)
+
+int _evaluate(int i, int n)
 {
-	if ((root * root) == num)
-		return (root);
+	if (n == 0 || n == 1)
+		return (n);
+	else if (i * i < n)
+		return (_evaluate(i + 1, n));
+	else if (i * i == n)
+		return (1);
+	return (-1);
+	return (-1);
 }
