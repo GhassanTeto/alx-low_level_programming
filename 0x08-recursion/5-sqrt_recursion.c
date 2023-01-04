fce2cde798d0eb5cd5314918b60ec6e28fd45432
GhassanTeto <ghassanteto19@gmail.com>
2023-01-04 14:28:20 -0800

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,3 +17,18 @@ int _evaluate(int i, int n)
 	else
 		return (-1);
 }
+
+/**
+ * _sqrt_recursion - evaluate sqrt
+ * @n: interger
+ * Return: Sgrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+	else
+		return (_evaluate(i, n));
+}
